2ea98238be46f66335685244a8ed1c9b9f41de5b
rickdeve <moreno234002@gmail.com>
2020-04-01 15:57:23 -0600

    project start

---
+++ src/app/components/pages/main/main.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import country from "world-map-country-shapes";
+import { NovelCovid } from 'novelcovid';
+@Component({
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+    mapCountries: any = '';
+    listCountries = [];
+    countryActive: any = 'MX';
+    cases: any = '';
+    deaths: any = '';
+    recovered: any = '';
+    casesWorld: any = '';
+    deathsWorld: any = '';
+    recoveredWorld: any = '';
+    url: string = '';
+    track = new NovelCovid();
+    constructor() { }
+
+    ngOnInit() {
+        this.mapCountries = country.map(async country => {
+            this.listCountries.push(country);
+        });
+        this.track.countries('MX').then(response => {
+            this.cases = response['cases'];
+            this.recovered = response['recovered'];
+            this.deaths = response['deaths'];
+        })
+        this.track.all().then(response => {
+            console.log(response);
+            this.casesWorld = response['cases'];
+            this.recoveredWorld = response['recovered'];
+            this.deathsWorld = response['deaths'];
+        })
+        this.url = 'https://www.countryflags.io/'+this.countryActive+'/flat/64.png';
+    }
+    selectCountry(id_country) {
+        this.url = 'https://www.countryflags.io/'+id_country+'/flat/64.png';
+        this.track.countries(id_country).then(response => {
+            this.cases = response['cases'];
+            this.recovered = response['recovered'];
+            this.deaths = response['deaths'];
+        })
+        console.log(this.track.all);
+        var hola = this.track.baseURL;
+        this.countryActive = id_country;
+        console.log(id_country);
+    }
+
+}
